38dc89afc7fa7fd953feba724aaf1ef6b12e7950
Sarthak <goyalsarthak003@gmail.com>
2023-06-26 19:44:16 +0530

    final-chaanges

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const messageRoutes = require("./routes/messages");
 const app = express();
 const socket = require("socket.io");
 require("dotenv").config();
-
+const BASE_URL=process.env.BASE_URL;
 app.use(cors());
 app.use(express.json());
 
@@ -24,7 +24,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "${BASE_URL}",
     credentials: true,
   },
 });
